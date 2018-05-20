d37094e348c84f117b510636d5687ac926aa62ee
Prashant Bhushan <pbhushan.email.arizona.edu>
2018-05-19 23:31:29 -0700

    Push to Git
    Push to Git

---
+++ household_power_consumption/plot1.R
@@ -0,0 +1,9 @@
+library ("data.table")
+setwd("~/R/RDocuments/household_power_consumption/")
+data_full <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+
+data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+hist(data1$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
